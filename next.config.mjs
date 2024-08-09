48aaced6fe79b786a566c77ff2b523011af03514
Vechy <verachinwa@gmail.com>
2024-08-09 04:18:04 -0700

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                port: '',
-                pathname: '/destinyfelixkiisi/image/upload/w_65,h_65/personal/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dtn6bfvnz/image/upload/**", // Update this to match your actual cloud name and folder structure
+      },
+    ],
+  },
 };
 
 export default nextConfig;
